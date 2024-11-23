ce64e8d78b8e3fe29a33165717739310950f3c19
Yur@Fiv3 <uraporskalo@gmail.com>
2024-11-23 15:20:34 +0200

    Added footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-console.log('Hello world');
+const movies = document.querySelector('.movies__container');
+
+
+fetch('https://www.omdbapi.com/?apikey=5a1ce3c3&s=batman')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.Search){
+            data.Search.forEach(movie => {
+                const movieCard = `<div class="movie__card">
+                        <img 
+                        src="${movie.Poster}" 
+                        alt="" 
+                        class="movie__poster"
+                        />
+                        <h3 class="movie__title">${movie.Title}</h3>
+                        <p class="movie__year">Year: ${movie.Year}</p>
+                        <p class="movie__type">Type: ${movie.Type}</p>
+                    </div>`
+                    movies.innerHTML += movieCard;
+            });
+
+        }
+    })
